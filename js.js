7cbc2f4c7ae4f5c6a729e110f56c5cca5347df28
farhat fakhar <farhatbibiva@gmail.com>
2024-06-14 22:30:28 +0500

    initial commit

---
+++ js.js
@@ -0,0 +1,29 @@
+//  owl carousel 
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      center: true,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: false,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+  });
+
+
+//  
+
+
+
